9d7171b555ed2eab1e89260b1bdce21202b43c64
Jmirez2022 <Jazi.2000@hotmail.com>
2023-03-21 21:59:16 -0500

    part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-function App() {
+export default function ButtonAppBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: 'rgb(79, 68, 245)'}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
+            My Music App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <form className='formLogin'>
+        <TextField 
+          id='standard-basic'
+          label="Username *" 
+          variant="standard"
+          sx={{ 
+            width: 400,
+            marginTop: '20px' 
+          }}
+          />
+        <TextField 
+          id='standard-basic'
+          label="Password *" 
+          variant="standard"
+          sx={{ 
+            width: 400,
+            marginTop: '20px' 
+          }}
+          />
+        <div>
+          <a href='#' path=''>
+          <Button 
+          variant="contained" 
+          href="#" 
+          sx={{ 
+            width: 400,
+             backgroundColor:'rgb(79, 68, 245)',
+             marginTop: '30px'
+            }}
+          >
+            LOGIN
+          </Button>
+          </a>               
+        </div>
+        
+      </form>
+    </Box>
+      
   );
 }
-
-export default App;
